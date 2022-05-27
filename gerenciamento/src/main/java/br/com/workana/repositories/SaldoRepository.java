2778d7e682a7bcc8f14ee389bfa3240d85faab0d
Gustavo Henrique Batista <gu.henrique.batista77@gmail.com>
2022-05-27 19:30:46 -0300

    devop

--- gerenciamento/src/main/java/br/com/workana/repositories/SaldoRepository.java
+++
@@ -1,8 +0,0 @@
-package br.com.workana.repositories;
-
-import br.com.workana.Entities.Saldo;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.Optional;
-
-public interface SaldoRepository extends JpaRepository<Saldo, Double> {}
