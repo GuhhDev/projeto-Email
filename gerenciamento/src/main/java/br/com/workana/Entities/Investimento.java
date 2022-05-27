812b648ab4d667d1891c9c249bf2f8f5921143a6
Gustavo Henrique Batista <gu.henrique.batista77@gmail.com>
2022-05-27 19:32:09 -0300

    Revert "devop"
    This reverts commit 2778d7e682a7bcc8f14ee389bfa3240d85faab0d.

---
+++ gerenciamento/src/main/java/br/com/workana/Entities/Investimento.java
@@ -0,0 +1,24 @@
+package br.com.workana.Entities;
+
+import com.sun.xml.bind.v2.model.core.ID;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "investimento")
+@Getter
+@Setter
+public class Investimento {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Integer investimento;
+
+    public Investimento() {}
+
+    public Investimento(Investimento investimento) {}
+}
---
+++ gerenciamento/src/main/java/br/com/workana/repositories/InvestimentoRepository.java
@@ -0,0 +1,10 @@
+package br.com.workana.repositories;
+
+import br.com.workana.Entities.Investimento;
+import com.sun.xml.bind.v2.model.core.ID;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+public interface InvestimentoRepository extends JpaRepository<Investimento, Long> {}
