f742ecc9538fdb0e860d533ee5c480ba10af5040
DESKTOP-SI63UBC\guhen <gu.henrique.batista77@gmail.com>
2022-05-26 23:46:25 -0300

    primeiro commit do projeto

---
+++ gerenciamento/src/main/java/br/com/workana/services/SaldoService.java
@@ -0,0 +1,16 @@
+package br.com.workana.services;
+
+import br.com.workana.Entities.Investimento;
+import br.com.workana.Entities.Saldo;
+import br.com.workana.repositories.InvestimentoRepository;
+import br.com.workana.repositories.SaldoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+
+@Service
+public class SaldoService {
+
+}
